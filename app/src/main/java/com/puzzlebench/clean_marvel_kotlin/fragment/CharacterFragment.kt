5aab5a42a90e98899a9648f794328db4e46020a3
Nico <nicolas.lb@globant.com>
2019-03-29 15:43:04 -0300

    Fixed some errors

--- app/src/main/java/com/puzzlebench/clean_marvel_kotlin/fragment/CharacterFragment.kt
+++ app/src/main/java/com/puzzlebench/clean_marvel_kotlin/fragment/CharacterFragment.kt
@@ -1,12 +1,11 @@
 package com.puzzlebench.clean_marvel_kotlin.fragment
 
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.Window
+import android.util.DisplayMetrics
+import android.view.*
 import com.puzzlebench.clean_marvel_kotlin.R
 import com.puzzlebench.clean_marvel_kotlin.data.service.CharacterServicesImpl
 import com.puzzlebench.clean_marvel_kotlin.domain.model.Character
@@ -22,18 +21,21 @@ class CharacterFragment : DialogFragment() {
     companion object {
         private lateinit var character: Character
         private const val DOT = "."
-        private lateinit var theactivity: MainActivity
+        private const val EMPTY_TEXT = ""
+        private const val NO_DESCRIPTION = "No available description"
+        private const val SCREEN_PERCENTAGE = .925
+        private lateinit var mainActivity: MainActivity
 
         fun newInstance(character: Character, activity: MainActivity): CharacterFragment {
             this.character = character
-            theactivity = activity
+            mainActivity = activity
             return CharacterFragment()
         }
     }
 
     fun init() {
         val getSingleCharacterServiceUseCase = GetSingleCharacterServiceUseCase(CharacterServicesImpl(), character.id)
-        val presenter = FragmentPresenter(FragmentView(theactivity), getSingleCharacterServiceUseCase)
+        val presenter = FragmentPresenter(FragmentView(mainActivity), getSingleCharacterServiceUseCase)
         presenter.init(this)
     }
 
@@ -47,8 +49,10 @@ class CharacterFragment : DialogFragment() {
                               savedInstanceState: Bundle?): View? {
         val frag = inflater.inflate(R.layout.fragment_character, container, false)
         frag.text_characterName.text = character.name
-        frag.text_characterDesc.text = character.description
+        frag.text_characterDesc.text = if (character.description != EMPTY_TEXT) character.description else NO_DESCRIPTION
         frag.imageView.getImageByUrl(character.thumbnail.path + DOT + character.thumbnail.extension)
+        frag.imageView.layoutParams.width =
+                ((mainActivity.getSystemService(Context.WINDOW_SERVICE)as WindowManager).defaultDisplay.width* SCREEN_PERCENTAGE).toInt()
         return frag
     }
 
--- app/src/main/java/com/puzzlebench/clean_marvel_kotlin/presentation/extension/ImageExtensions.kt
+++ app/src/main/java/com/puzzlebench/clean_marvel_kotlin/presentation/extension/ImageExtensions.kt
@@ -1,8 +1,9 @@
 package com.puzzlebench.clean_marvel_kotlin.presentation.extension
 
 import android.widget.ImageView
-import com.squareup.picasso.Picasso
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 fun ImageView.getImageByUrl(url: String) {
-    Picasso.with(context).load(url).fit().centerCrop().into(this)
+    Glide.with(context).load(url).apply(RequestOptions().centerCrop()).into(this)
 }
