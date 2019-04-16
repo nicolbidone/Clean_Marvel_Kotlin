59cce684b0d88fd0bb02101719c3b59f536eb852
Nico <nicolas.lb@globant.com>
2019-04-16 14:39:18 -0300

    Toolbar implemented

--- app/src/main/java/com/puzzlebench/clean_marvel_kotlin/presentation/MainActivity.kt
+++ app/src/main/java/com/puzzlebench/clean_marvel_kotlin/presentation/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.puzzlebench.clean_marvel_kotlin.presentation
 
 import android.os.Bundle
+import android.support.v4.content.res.ResourcesCompat
+import android.view.Menu
 import com.puzzlebench.clean_marvel_kotlin.R
 import com.puzzlebench.clean_marvel_kotlin.data.service.CharacterServicesImpl
 import com.puzzlebench.clean_marvel_kotlin.data.service.CharacterStoredImpl
@@ -28,6 +30,12 @@ open class MainActivity : BaseRxActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val typeface = ResourcesCompat.getFont(applicationContext, R.font.marvel_regular)
+        toolbar_layout.setCollapsedTitleTypeface(typeface)
+        toolbar_layout.setExpandedTitleTypeface(typeface)
+
+        setSupportActionBar(toolbar)
+
         fab_download.setOnClickListener {
             presenter.requestGetCharacters()
         }
@@ -41,6 +49,11 @@ open class MainActivity : BaseRxActivity() {
         }
 
         presenter.init()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 
     private fun realmClean() {
